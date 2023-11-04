79fb8ced819a146f31b3f6168ed6b333028eb76f
zjg_lz <3557727504@qq.com>
2023-11-04 02:46:25 +0000

    use chatglm api for twice QA

--- lz/query.py
+++ lz/query.py
@@ -1,15 +1,21 @@
-from transformers import AutoTokenizer, AutoModel
 import json 
 import torch
 from tqdm import tqdm
 import random
 import argparse
+# from langchain.llms import ChatGLM
+from chatglm3.ChatGLM import ChatGLM
+from typing import List, Optional
 
-device = 'cuda:2'
-chatglm_path = "chatglm3-6b/chatglm3-6b"
-# chatglm_path = "chatglm2-6b-hug/"
-chatglm = AutoModel.from_pretrained(chatglm_path, trust_remote_code=True, device=device)
-chatglm = chatglm.eval()
+endpoint_url = ("http://127.0.0.1:29501")
+
+config = {
+        "temperature": 0.05,
+        "top_p": 0.6,
+        "threshold" : -40,
+        "max_token" : 4096,
+        # "with_history": True,
+    }
 
 def seed_everything(seed):
     torch.manual_seed(seed)
@@ -19,28 +25,70 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.cuda.manual_seed_all(seed)
 
-def get_answer(data,prompt_template,chatglm,params,
+
+
+def clean_related_str(related_str:List, keyword: Optional[List]=[], threshold=-80):
+    """
+        Input:
+            related_str: List[str]
+        Return:
+            List[str]
+    """
+    if len(related_str) == 1:
+        return related_str
+
+    ## remove previous section (if it contains the keyword)
+    if len(keyword) > 0 and keyword[0][1] > threshold:
+        potential_section = keyword[0][0] + "\n"
+        for i in range(len(related_str)):
+            if potential_section in related_str[i]:
+                related_str[i] = potential_section + potential_section.join(related_str[i].split(potential_section)[1:])
+            
+    ## remove duplicate
+    tmp_str = "\n".join(related_str)
+    parts = tmp_str.split("\n")
+    # print(parts)
+    # print("--------------------------------")
+    seen = set()
+    deduplicated_parts = []
+    # 去重，同时保证去重后的顺序不变
+    for part in parts:
+        if part not in seen:
+            seen.add(part)
+            deduplicated_parts.append(part)
+
+    related_str = "\n".join(deduplicated_parts)
+    # print(related_str)
+    return related_str
+
+def get_answer(data,prompt_template,
                loop = True) -> str:
     """
     通过 ChatGLM 进行QA
     """
 
+    system_info = {"role": "system", "content": "你是一位智能汽车说明的问答助手，你将根据节选的说明书的信息，完整地回答问题。"}
+    chatglm = ChatGLM(
+        endpoint_url=endpoint_url,
+        history=[system_info],
+        **config,
+        model_kwargs={"sample_model_args": False},
+    )
+
     inputs = {
         "question": data["question"],
-        "info":data["related_str"]
+        "info":clean_related_str(data["related_str"], data["keyword"])
         }
 
-    tokenizer = AutoTokenizer.from_pretrained(chatglm_path, trust_remote_code=True)
-    system_info = {"role": "system", "content": "你是一位智能汽车说明的问答助手，你将根据节选的说明书的信息，完整地回答问题。"}
-
     # Execute the chain
-    user_info = ""
-    for i in range(len(inputs["info"])):
-        user_info += "第{}条相关信息：\n{}\n".format(i+1,inputs["info"][i]) 
-    response0, history = chatglm.chat(tokenizer,prompt_template[0].format(inputs["question"],user_info), history=[system_info],**params)
+    user_info = inputs["info"]
+    # for i in range(len(inputs["info"])):
+    #     user_info += "第{}条相关信息：\n{}\n".format(i+1,inputs["info"][i]) 
+
+    response0 = chatglm(prompt_template[0].format(inputs["question"],user_info))
     # print(prompt_template[0].format(inputs["question"],user_info))
     if loop:
-        response, history = chatglm.chat(tokenizer,prompt_template[1].format(inputs["question"]), history=history,**params)
+        response = chatglm(prompt_template[1].format(inputs["question"],response0))
 
     return [response0,response]
 
@@ -75,8 +123,8 @@ def main(opt):
         json_data = file.read()
     datas = json.loads(json_data)
 
-    prompt_template = ["""请根据说明书中提取的已知信息回答问题。注意，相关信息的顺序不决定它的重要性，问题可能出现错别字，例如反光境是反光镜。问题是：{} {} 答案是：""",
-        """请尽可能简要地总结先前的回答，只保留与问题最相关的部分，在总结中不要重复问题。问题是：{} 答案是："""]
+    prompt_template = ["""请根据说明书中提取的已知信息，简洁准确地回答问题。注意，相关信息的顺序不决定它的重要性，问题可能出现错别字，例如反光境是反光镜。问题是：{} {} 答案是：""",
+        """请根据问题尽可能简要地总结先前的回答，去掉与问题不相关的部分。问题是：{} \n先前的回答：\n{} 答案是："""]
 
     max_length = 4096
     top_p      = 0.6
@@ -86,7 +134,7 @@ def main(opt):
     seed_everything(2023)
     results = []
     for data in tqdm(datas,desc="question"):
-        ret = get_answer(data,prompt_template,chatglm,params)
+        ret = get_answer(data,prompt_template)
         sample = {"question": data["question"], "answer_1": ret[0], "answer_2": ret[1]}
         results.append(sample)
         print(sample["answer_1"])
