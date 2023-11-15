c0e876b01097297baf59cafa2726e4737d8852fe
we1k <819449637@qq.com>
2023-11-15 08:07:49 +0000

    v1

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 # builder for pdf2html
-# sudo apt install ./builder/pdf2htmlEX.deb
-# pip install html2text
+sudo apt install ./builder/pdf2htmlEX.deb
+pip install html2text
 
 # pdf2html
-# pdf2htmlEX --embed cfijo --dest-dir pdf_output data/QA.pdf
+pdf2htmlEX --embed cfijo --dest-dir pdf_output data/QA.pdf
 
 # html2text
-# html2text pdf_output/QA.html utf-8 --ignore-links --escape-all > data/raw.txt
+html2text pdf_output/QA.html utf-8 --ignore-links --escape-all > data/raw.txt
 
 # run file for python
-python src/preprocess.py
+python retrieve_info.py --embedding_model stella
